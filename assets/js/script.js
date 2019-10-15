a2efbfecdd68b9948b86073f8efca0471f286dab
sneachda <savela.aleks@gmail.com>
2019-10-15 10:41:56 +0100

    countdwon clock updated in JS, when rules clicked, clock is paused.

--- assets/js/script.js
+++ assets/js/script.js
@@ -185,17 +185,20 @@ let span = document.getElementsByClassName("close")[0];
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
+  clearInterval(countdown);
 };
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+  countdown = setInterval(startClock, 1000);
 };
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target === modal) {
     modal.style.display = "none";
+    countdown = setInterval(startClock, 1000);;
   }
 };
 
